ef5e53fccd11fbba276bf996c8ae5027badd2657
Peter Strömberg <pez@pezius.com>
2023-10-12 10:46:11 +0200

    Prepend docker image name with `cospaia`

--- examples/compose.clj
+++ examples/compose.clj
@@ -1,21 +1,25 @@
-#!/usr/bin/env -S bb -cp ./bin
+#!/usr/bin/env -S bb -cp ./examples
 (ns compose
   (:require [babashka.fs :as fs]
             [babashka.process :as p]))
+
+(defonce compose-tmp (name (gensym "compose-")))
 
 (defn compose! [output-path]
   (let [output-dir (-> output-path fs/file fs/parent)]
     (when output-dir
       (fs/create-dirs output-dir))
-    (let [tmp-dir (fs/path (fs/temp-dir) (name (gensym "compose-")))
-          pango (str (fs/path tmp-dir "pango.png"))
-          montage-1 (str (fs/path tmp-dir "montage-1.png"))
-          montage-2 (str (fs/path tmp-dir "montage-2.png"))]
+    (let [tmp-dir (fs/path (fs/temp-dir) compose-tmp)
+          pango-path (str (fs/path tmp-dir "pango.png"))
+          montage-1-path (str (fs/path tmp-dir "montage-1.png"))
+          montage-2-path (str (fs/path tmp-dir "montage-2.png"))]
       (fs/create-dirs tmp-dir)
-      (println "Writing pango output to: " pango)
+      (println "Writing pango output to: " pango-path)
       (p/sh "convert" "-background" "white" "-size" "1140x"
             (str "pango:<span font_size='36000'>"
-                 "<b>magick-pango-babashka<tt>:latest</tt></b></span>"
+                 "<b>"
+                 "<span foreground='#9370DB'>cospaia</span>"
+                 " / magick-pango-babashka<tt>:latest</tt></b></span>"
                  "\n"
                  "<span font_size='28000'>"
                  "A Docker image to power your pictures + text "
@@ -25,21 +29,22 @@
                  "cospaia/magick-pango-babashka examples/compose.clj "
                  "output/composition.png</tt>")
             "-bordercolor" "white" "-border" "30"
-            pango)
-      (println "Writing first montage output to: " montage-1)
+            pango-path)
+      (println "Writing first montage output to: " montage-1-path)
       (p/sh "montage" "-resize" "350x"
-            "examples/assets/ImageMagick.png" "examples/assets/pango-name.png"
             "examples/assets/babashka.png"
+            "examples/assets/ImageMagick.png"
+            "examples/assets/pango-name.png"
             "-geometry" "+0+0" "-gravity" "center"
             "-background" "white" "-tile" "x1"
             "-mode" "Concatenate"
-            montage-1)
-      (println "Writing second montage output to: " montage-2)
-      (p/sh "convert" montage-1
+            montage-1-path)
+      (println "Writing second montage output to: " montage-2-path)
+      (p/sh "convert" montage-1-path
             "-gravity" "north" "-extent" "1200x383+0-20"
-            montage-2)
+            montage-2-path)
       (println "Writing result output to: " output-path)
-      (p/sh "montage" montage-2 pango
+      (p/sh "montage" montage-2-path pango-path
             "-tile" "x2" "-mode" "concatenate"
             output-path))))
 
